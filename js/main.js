6c20f3fc3072762b58a1052647feb393c0af1403
Matteo Ponza <matteoponza@gmail.com>
2025-04-16 21:04:21 +0200

    feat: Add new sample site with hugo

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // when document is loaded
-    console.log("[loaded]")
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
